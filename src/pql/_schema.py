220f61f3d6d6723e02dbbcc55062c59d3fd732e6
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-04-01 13:39:45 +0200

    add conversion path to sqlglot mapping schema for Schema

--- src/pql/_schema.py
+++ src/pql/_schema.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING, Self
 
 import pyochain as pc
+from sqlglot.schema import MappingSchema
 
 from ._datatypes import DataType
 
@@ -17,3 +18,13 @@ class Schema(pc.Dict[str, DataType]):
     def from_frame(cls, frame: DuckDBPyRelation) -> Self:
         dtypes = pc.Iter(frame.dtypes).map(DataType.from_duckdb)
         return pc.Iter(frame.columns).zip(dtypes, strict=True).collect(cls)
+
+    def to_mapping_schema(self, table_name: str) -> MappingSchema:
+        type_map = (
+            self
+            .items()
+            .iter()
+            .map_star(lambda k, v: (k, v.raw.sql(dialect="duckdb")))
+            .collect(dict)
+        )
+        return MappingSchema({table_name: type_map}, dialect="duckdb")
