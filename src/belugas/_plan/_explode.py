8de8a5b5874af4ba0b9e2e97f1c7266ea15cb156
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-05-10 12:50:59 +0200

    refactor: optimize `_plan::_explode::explode` to avoid multiple passes on the same sequence of expressions

--- src/belugas/_plan/_explode.py
+++ src/belugas/_plan/_explode.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 from collections.abc import Iterable
-from typing import TYPE_CHECKING
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, NamedTuple
 
-from pyochain import Dict, Iter, Seq
+from pyochain import Dict, Iter
 from sqlglot import exp
 
 from ._meta import Tables, resolve_all
@@ -21,87 +22,78 @@ def explode(
     columns: TryIter[IntoExprColumn],
     more_columns: Iterable[IntoExprColumn],
 ) -> exp.Union:
-    from .._funcs import col, lit
+    from .._funcs import col
 
-    to_explode_names = (
+    to_explode = (
         resolve_all(schema, columns, more_columns, {})
         .iter()
-        .map(lambda r: r.name)
-        .collect()
-    )
-    to_explode = to_explode_names.iter().map(col).collect()
-    target = (
-        to_explode.first()
-        if to_explode.length() == 1
-        else (to_explode.first().list.zip(*to_explode.iter().skip(1), lit(1).eq(1)))
-    )
-
-    zipped_index = (
-        to_explode_names
-        .iter()
         .enumerate()
-        .map_star(lambda idx, name: (name, idx + 1))
+        .map_star(lambda idx, r: (r.name, IndexedExpr(idx + 1, col(r.name))))
         .collect(Dict)
     )
     is_single_explode = to_explode.length() == 1
+    target = (
+        to_explode
+        .values()
+        .iter()
+        .into(_get_target, is_single_explode=is_single_explode)
+    )
 
     cond = target.is_not_null().and_(target.list.length().gt(0))
+    projections = ExplodedProjections(
+        schema.keys(), to_explode, target, is_single_explode
+    )
 
     rhs = (
         exp
-        .select(
-            *_proj(
-                schema.keys(),
-                zipped_index,
-                to_explode_names,
-                target,
-                nested=False,
-                is_single_explode=is_single_explode,
-            )
-        )
+        .select(*projections.transform(nested=False))
         .from_(Tables.EXPLODE_SRC, copy=False)
         .where(cond.not_().inner, copy=False)
     )
     return (
         exp
-        .select(
-            *_proj(
-                schema.keys(),
-                zipped_index,
-                to_explode_names,
-                target,
-                nested=True,
-                is_single_explode=is_single_explode,
-            )
-        )
+        .select(*projections.transform(nested=True))
         .from_(Tables.EXPLODE_SRC, copy=False)
         .where(cond.inner, copy=False)
         .pipe(exp.union, rhs, copy=False)
     )
 
 
-def _proj(  # noqa: PLR0913
-    columns: PyoIterable[str],
-    zipped_index: Dict[str, int],
-    to_explode_names: Seq[str],
-    target: Expr,
-    *,
-    nested: bool,
-    is_single_explode: bool,
-) -> Iter[exp.Expr]:
-    from .._funcs import col, lit, unnest
+def _get_target(exprs: Iter[IndexedExpr], *, is_single_explode: bool) -> Expr:
+    from .._funcs import lit
 
-    def _project_col(name: str, replace: Expr) -> Expr:
-        match (nested, name in to_explode_names):
-            case (True, True):
-                if is_single_explode:
-                    return replace.alias(name)
-                field = zipped_index.get_item(name).unwrap()
-                return replace.struct.extract(field).alias(name)
-            case (False, True):
-                return lit(None).alias(name)
-            case _:
-                return col(name)
+    first_expr = exprs.next().unwrap().expr
+    if is_single_explode:
+        return first_expr
+    return first_expr.list.zip(*exprs.map(lambda ie: ie.expr), lit(1).eq(1))
 
-    replace = unnest(target) if nested else lit(None)
-    return columns.iter().map(lambda name: _project_col(name, replace).inner)
+
+class IndexedExpr(NamedTuple):
+    idx: int
+    expr: Expr
+
+
+@dataclass(slots=True)
+class ExplodedProjections:
+    columns: PyoIterable[str]
+    to_explode: Dict[str, IndexedExpr]
+    target: Expr
+    is_single: bool
+
+    def transform(self, *, nested: bool) -> Iter[exp.Expr]:
+        from .._funcs import col, lit, unnest
+
+        def _project_col(name: str, replace: Expr) -> Expr:
+            match (nested, name in self.to_explode):
+                case (True, True):
+                    if self.is_single:
+                        return replace.alias(name)
+                    field = self.to_explode.get_item(name).unwrap().idx
+                    return replace.struct.extract(field).alias(name)
+                case (False, True):
+                    return lit(None).alias(name)
+                case _:
+                    return col(name)
+
+        replace = unnest(self.target) if nested else lit(None)
+        return self.columns.iter().map(lambda name: _project_col(name, replace).inner)
